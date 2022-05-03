ca3fc7b5eaf45ecfd1713edfbfdec41a7a90e195
jigmi <87882680+jigmi@users.noreply.github.com>
2022-05-03 20:39:31 +1000

    Create StudentResultTree.java

---
+++ StudentResultTree.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    //Don't touch this method!
+    private static void printArray(int[] a) {
+        System.out.println(Arrays.toString(a));
+    }
