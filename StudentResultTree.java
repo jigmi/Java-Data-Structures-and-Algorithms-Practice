d55ff2b24d218b4918ba0ce2df7f1876ce5f4d9a
jigmi <87882680+jigmi@users.noreply.github.com>
2022-08-02 16:34:48 +1000

    Update StudentResultTree.java

--- StudentResultTree.java
+++ StudentResultTree.java
@@ -1,9 +1,29 @@
 import java.util.Arrays;
-
 public class BubbleSort {
-
-    //Don't touch this method!
     private static void printArray(int[] a) {
         System.out.println(Arrays.toString(a));
     }
- woah what ru doing
+    public static int[] bubbleSort(int[] data) {
+        boolean status = true;
+        while (status) {
+            status = false;
+            for (int a = 0; a < data.length-1;a++) {
+                if (data[a] > data[a+1]) {
+                    int temp = data[a];
+                    data[a] = data[a+1];
+                    data[a+1] = temp;
+                    status = true;
+                    printArray(data);
+                }          
+            }
+        }
+        return data;
+    }
+    public static void main(String[] args) {
+        int[] testData = {45, 93, 33, 55};
+        System.out.println("Sorting.");
+        testData = bubbleSort(testData);
+        System.out.println("After sorting the array is: ");
+        printArray(testData);
+    }
+}
